54ed946f95fe9b4cadbe6af19f5c1c79ee09a914
Denver Coneybeare <dconeybe@google.com>
2025-02-14 15:53:53 -0500

    llll.cc: some progress

--- llll.cc
+++ llll.cc
@@ -1,4 +1,5 @@
 #include <atomic>
+#include <cinttypes>
 #include <thread>
 
 struct node {
@@ -8,6 +9,19 @@ struct node {
 
 std::atomic<node> head;
 
+void ll_append(int value) {
+  const node head_copy = head.load();
+  const node * cur = &head_copy;
+  while (cur->next) {
+    cur = cur->next;
+  }
+}
+
 int main(int argc, char** argv) {
+  (void)argc;
+  (void)argv;
+
+  head.store({ .value = 0, .next = nullptr });
+
   return 0;
 }
